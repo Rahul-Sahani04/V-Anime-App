4c2e711bf8936b5a3804535e3fd38ff1214697f5
Rahul <110347707+Rahul-Sahani04@users.noreply.github.com>
2024-06-16 12:58:37 +0530

    Update WatchAnime page to set current episode and display it

--- src/pages/WatchAnime.js
+++ src/pages/WatchAnime.js
@@ -86,8 +86,10 @@ function Watch() {
     }
   };
   const fetchM3U8 = async (id) => {
+    setCurrentEp(id.split("-").pop());
     const data = await fetch(`${API_ENDPOINT}/meta/anilist/watch/${id}`); // Zoro
     const anime_link = await data.json();
+    console.log("Anime: ", anime_link);
     const WatchUrl = anime_link.sources[anime_link.sources.length - 1].url;
     setWatchUrl(WatchUrl);
   };
@@ -104,6 +106,7 @@ function Watch() {
     setQuery(Query);
 
     if (episode) {
+      // setCurrentEp(episode.split("-").pop());
       fetchM3U8(episode);
     }
     if (Query === undefined || Query === null || Query === "") {
@@ -266,6 +269,10 @@ function Watch() {
                   d="M15,180h263.787l-49.394,49.394c-5.858,5.857-5.858,15.355,0,21.213C232.322,253.535,236.161,255,240,255  s7.678-1.465,10.606-4.394l75-75c5.858-5.857,5.858-15.355,0-21.213l-75-75c-5.857-5.857-15.355-5.857-21.213,0  c-5.858,5.857-5.858,15.355,0,21.213L278.787,150H15c-8.284,0-15,6.716-15,15S6.716,180,15,180z"
                 />
               </svg>
+              <p className="text-sm">
+                Currently Watching: <br />
+                Episode {currentEp}
+              </p>
             </div>
           </h2>
 
